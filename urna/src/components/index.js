e2c4f90b6ab7c57cbfcb1b9be6e83457af5bea70
Lucas-Carvalho1607 <82171494+Lucas-Carvalho1607@users.noreply.github.com>
2023-11-07 22:53:34 +0000

    imagem

--- urna/src/components/index.js
+++ urna/src/components/index.js
@@ -7,10 +7,35 @@ export default function TelaInicial () {
             <div class="tela">
 
             </div>
+            <div class="titutecla">
+
+             <div class="titulo">
+                <img src=""></img>
+                <h1>Injustiça Eleitoral <br/> do Senai </h1>
+
+            </div>
 
             <div class="teclado">
 
+
+                <div class="um">
+                    <h2>1</h2>
+
+                    <div class="dois">
+                    <h2>2</h2>
+
+                    </div>
+
+                    <div class="tres">
+                    <h2>3</h2>
+
+                    </div>
+
+                </div>
+
             </div>
+            </div>
+
 
         </div>
 
