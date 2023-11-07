528a710d356a584ec277d9b3dd5195c1ed3ab4f3
Roberta <142239752+RobertaTardelli@users.noreply.github.com>
2023-11-07 00:37:08 +0000

    inicio

---
+++ urna/src/App.js
@@ -0,0 +1,18 @@
+import './App.css';
+import TelaInicial from './components';
+
+function App() {
+
+  return (
+    <div className="App">
+      <header className="App-header">
+       
+        <p>
+          <TelaInicial/>
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
---
+++ urna/src/components/index.js
@@ -0,0 +1,19 @@
+import "./style.css";
+
+export default function TelaInicial () {
+
+    return (
+        <div class="urna">
+            <div class="tela">
+
+            </div>
+
+            <div class="teclado">
+
+            </div>
+
+        </div>
+
+    );
+
+}
